8ea1d2a7e3fb9a58e8c62099a7dbb76188a2fb3f
Tamanna-akter <atamanna577@gmail.com>
2022-05-26 22:27:33 +0600

    extra two section in home page are done

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='bg-primary px-10 py-14 '>
+        <div className='bg-neutral px-10 py-14 '>
           <div className='text-center pb-14 text-white'>
-            <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
+            {/* <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
               Contact Us
-            </p>
+            </p> */}
             <h1 className='text-4xl'>Stay connected with us</h1>
           </div>
           <div className='grid grid-cols-1 justify-items-center gap-5'>
+            <input
+              type='text'
+              placeholder='Name'
+              className='input w-full max-w-md'
+            />
             <input
               type='text'
               placeholder='Email Address'
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './About';
 import Banner from './Banner';
+import Contact from './Contact';
 import Summary from './Summary';
 import Tools from './Tools';
 
@@ -11,6 +12,7 @@ const Home = () => {
            <Tools></Tools>
            <Summary></Summary>
            <About></About>
+           <Contact></Contact>
         </div>
     );
 };
